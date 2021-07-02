2a3dc27e5b6e09f56b40e4682428227ca826afc4
nbrichau <h.nicolas2000@gmail.com>
2021-07-02 15:23:00 +0200

    Try to modify the baked models

--- src/main/java/com/infinityraider/agricraft/plugins/bayoublues/BayouBluesPlugin.java
+++ src/main/java/com/infinityraider/agricraft/plugins/bayoublues/BayouBluesPlugin.java
@@ -1,17 +1,24 @@
 package com.infinityraider.agricraft.plugins.bayoublues;
 
+import com.google.common.collect.ImmutableList;
 import com.infinityraider.agricraft.AgriCraft;
 import com.infinityraider.agricraft.api.v1.AgriApi;
+import com.infinityraider.agricraft.api.v1.client.PlantQuadBakeEvent;
 import com.infinityraider.agricraft.api.v1.content.items.IAgriSeedItem;
 import com.infinityraider.agricraft.api.v1.plugin.AgriPlugin;
 import com.infinityraider.agricraft.api.v1.plugin.IAgriPlugin;
 import com.infinityraider.agricraft.reference.Names;
+import com.infinityraider.agricraft.render.plant.AgriPlantQuadGenerator;
+import com.infinityraider.infinitylib.render.tessellation.ITessellator;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.model.BakedQuad;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -19,6 +26,8 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+import java.util.List;
 
 @AgriPlugin(modId = Names.Mods.BAYOU_BLUES)
 public class BayouBluesPlugin implements IAgriPlugin {
@@ -38,7 +47,7 @@ public class BayouBluesPlugin implements IAgriPlugin {
             //So we have to check if the item in the main hand, because that's the one we want first
             // TODO: 01/07/2021 find a way to cancel this behaviour (we want the event when the right click is on the main hand)
             Item mainItem = player.getHeldItem(Hand.MAIN_HAND).getItem();
-            ItemStack stack = mainItem instanceof IAgriSeedItem? player.getHeldItem(Hand.MAIN_HAND) : player.getHeldItem(Hand.OFF_HAND);
+            ItemStack stack = mainItem instanceof IAgriSeedItem ? player.getHeldItem(Hand.MAIN_HAND) : player.getHeldItem(Hand.OFF_HAND);
             System.out.println("using stack " + stack);
             if (!stack.isEmpty() && stack.getItem() instanceof IAgriSeedItem) {
                 IAgriSeedItem seedItem = (IAgriSeedItem) stack.getItem();
@@ -70,6 +79,68 @@ public class BayouBluesPlugin implements IAgriPlugin {
         }
     }
 
+    public static void onQuadBakedVertex(PlantQuadBakeEvent event) {
+        System.out.println("event catch");
+//        IAgriRenderable renderplant = event.getPlant();
+//        if (renderplant instanceof IAgriPlant) {
+//            IAgriPlant agriPlant = ((IAgriPlant) renderplant);
+//            System.out.println("getting plant id : " + agriPlant.getId());
+//            if (agriPlant.isPlant() && agriPlant.getId().equals("upgrade_aquatic:purple_pickerelweed_plant")) {
+//                System.out.println("baking purple pickerelweed");
+//            }
+//        }
+        ImmutableList<BakedQuad> originalQuads = event.getOriginalQuads();
+        event.clearQuads();// we clear the quads as we don't want the originals
+
+        System.out.println("looping on quads");
+        for (BakedQuad originalQuad : originalQuads) {
+            //code adapted from https://forums.minecraftforge.net/topic/66893-112-is-there-a-way-to-color-bakedquads/?do=findComment&comment=321762
+//            if (originalQuad.getTintIndex() >= 0) {
+//                System.out.println("processing tint index " + originalQuad.getTintIndex());
+            int[] vertex = originalQuad.getVertexData();
+            VertexFormat format = DefaultVertexFormats.BLOCK;
+            int size = format.getIntegerSize();
+            int color = 0xFF0000FF;//format is ABGR
+            for (int i = 0; i < 4; i++) {
+                vertex[format.getOffset(i) / 4 + size * i] = color;//we change the color
+            }
+
+            BakedQuad newQuad = new BakedQuad(vertex,
+                    originalQuad.getTintIndex(),
+                    originalQuad.getFace(),
+                    originalQuad.getSprite(),
+                    originalQuad.applyDiffuseLighting());
+            event.injectQuads(newQuad);//we add the new quad. Unfortunately, they are invisible
+//            } else {
+//                System.out.println("injecting original");
+//                event.injectQuads(originalQuads);
+//            }
+        }
+        System.out.println("amount of quads " + event.getOutputQuads().size());
+    }
+
+    public static void onQuadBakedTessellator(PlantQuadBakeEvent event) {
+        AgriPlantQuadGenerator quadGenerator = (AgriPlantQuadGenerator) event.getQuadGenerator();
+        List<BakedQuad> originalQuads = event.getOriginalQuads();
+        event.clearQuads();
+
+        ITessellator tessellator = quadGenerator.getTessellator();
+
+        tessellator.startDrawingQuads();
+        tessellator.setFace((Direction) null);
+        tessellator.pushMatrix();
+
+        tessellator.setColorRGB(1, 0, 0);
+        tessellator.addQuads(originalQuads);
+        //when the quads are added, the color isn't applied to them
+
+        tessellator.popMatrix();
+        List<BakedQuad> quads = tessellator.getQuads();
+        event.injectQuads(quads);
+
+        tessellator.draw();
+    }
+
     @Override
     public boolean isEnabled() {
         return true;
@@ -88,11 +159,13 @@ public class BayouBluesPlugin implements IAgriPlugin {
     @Override
     public void onCommonSetupEvent(FMLCommonSetupEvent event) {
         MinecraftForge.EVENT_BUS.addListener(BayouBluesPlugin::onRightClickBlock);
+        MinecraftForge.EVENT_BUS.addListener(BayouBluesPlugin::onQuadBakedVertex);
+//        MinecraftForge.EVENT_BUS.addListener(BayouBluesPlugin::onQuadBakedTessellator);
     }
 
     @Override
     public void onClientSetupEvent(FMLClientSetupEvent event) {
-        Minecraft.getInstance().getBlockColors().register((x, world, pos, u) -> world != null && pos !=null ? 2129968 : 7455580, AgriCraft.instance.getModBlockRegistry().crop_plant);
+//        Minecraft.getInstance().getBlockColors().register((x, world, pos, u) -> world != null && pos !=null ? 2129968 : 7455580, AgriCraft.instance.getModBlockRegistry().crop_plant);
     }
 
 }
